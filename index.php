e99fca397e46bfca401d06c9491a8fd78c0b0958
imagodei2020 <imagodeidesign1989@gmail.com>
2022-08-20 02:44:44 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Imago Dei Design - Home</title>
+    <link rel="stylesheet" href="main.css">
+  </head>
+  <body>
+    <nav class="main_nav">
+      <ul class="main_nav_items">
+        <li class="main_nav_item"><a href="index.php">Home</a></li>
+      </ul>
+    </nav>
+  </body>
+</html>
